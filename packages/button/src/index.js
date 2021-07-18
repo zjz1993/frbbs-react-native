70070c3edc8ed07ff288982bbc5473bd19983d87
赵俊哲 <1429595365@qq.com>
2021-07-18 15:25:43 +0800

    update: button组件修改

---
+++ packages/button/src/index.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Text, View } from 'react-native';
+const Button = ({ title }) => {
+    return <View><Text>ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>;
+};
+export default Button;
