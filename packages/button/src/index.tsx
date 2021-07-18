cce6dd77f0be82e6eae9260e80c9647b1ace0099
赵俊哲 <1429595365@qq.com>
2021-07-18 16:52:14 +0800

    update: 修改入口

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -4,6 +4,6 @@ type Props = {
     title?:string;
 }
 const Button:React.FC<Props> = ({title}) => {
-    return <View><Text>增加一些东西，这是我的ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>
+    return <View><Text style={{color:"white"}}>增加一些东西，这是我的ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>
 }
 export default Button;
