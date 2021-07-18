b66156e64b3265ddd5586322fb650cb6374e3072
赵俊哲 <1429595365@qq.com>
2021-07-18 15:43:29 +0800

    update: 更新button组件

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Text, View } from 'react-native';
 const Button = ({ title }) => {
-    return <View><Text>ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>;
+    return <View><Text>这是我的ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>;
 };
 export default Button;
--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -4,6 +4,6 @@ type Props = {
     title?:string;
 }
 const Button:React.FC<Props> = ({title}) => {
-    return <View><Text>ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>
+    return <View><Text>这是我的ui库第一个组件</Text><Text>传过来的title是{title}</Text></View>
 }
 export default Button;
